351368def41d9019a3d629406331d044915bea43
Akshata <teggiakshata2002@gmail.com>
2025-06-17 22:51:00 +0530

    update

--- lib/constants.ts
+++ lib/constants.ts
@@ -5,4 +5,9 @@
 
 
 // API configuration
-export const API_BASE_URL = "http://192.168.0.164:5000"
+// export const API_BASE_URL = "http://192.168.0.164:5000"
+
+
+
+// API configuration
+export const API_BASE_URL = "http://192.168.43.161:5000"
